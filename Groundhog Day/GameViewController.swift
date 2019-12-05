49d8d2086788b0459b6c3a4eb4c6f401f7a216cb
Cole Elfstrom <cole.elfstrom.368@k12.friscoisd.org>
2019-12-05 09:48:47 -0600

    Preparing View

--- Groundhog Day/GameViewController.swift
+++ Groundhog Day/GameViewController.swift
@@ -15,41 +15,5 @@ class GameViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? // Load the SKScene from 'GameScene.sks'
-        {
-            if let scene = SKScene(fileNamed: "GameScene")
-            {
-                scene.scaleMode = .aspectFill // Set the scale mode to scale to fit the window
-                view.presentScene(scene) // Present the scene
-            }
-            
-            view.ignoresSiblingOrder = true
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
-
-    override var shouldAutorotate: Bool
-    {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask
-    {
-        if UIDevice.current.userInterfaceIdiom == .phone
-        {
-            return .allButUpsideDown
-        }
-        
-        else
-        {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool
-    {
-        return true
     }
 }
