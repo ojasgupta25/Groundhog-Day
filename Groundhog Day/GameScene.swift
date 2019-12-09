a819b8c72e8a1406550c8bcbeba97da140643846
Andrew Villarreal <andrewvillarreal2010@hotmail.com>
2019-12-09 09:22:31 -0600

    Timer

--- Groundhog Day/GameScene.swift
+++ Groundhog Day/GameScene.swift
@@ -2,7 +2,7 @@
 //  GameScene.swift
 //  Groundhog Day
 //
-//  Created by Garcia Jimenez, Alejandro J on 12/3/19.
+//  Created by Garcia Jimenez, Alejandro J, Andrew Vilarreal, Cole, and Ojas on 12/3/19.
 //  Copyright © 2019 Garcia, Alejandro. All rights reserved.
 //
 
@@ -12,4 +12,13 @@ import GameplayKit
 class GameScene: SKScene
 {
     
+}
+
+struct Timer{
+    var time = 60//This is the set time they are allowed to play the game
+    //this function is the core to the count down timer loop
+    //(this should be placed in a loop in view controller)
+    mutating func countDown(penalty: Int){
+        time = time-(penalty)
+    }
 }
