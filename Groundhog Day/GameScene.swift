bb2a1558c4414e2caf9bcd4888fce05f712c27f6
Cole Elfstrom <cole.elfstrom.368@k12.friscoisd.org>
2019-12-11 10:04:39 -0600

    Timer completed and randomized moles completed, lots o stuff

--- Groundhog Day/GameScene.swift
+++ Groundhog Day/GameScene.swift
@@ -8,46 +8,9 @@
 
 import SpriteKit
 import GameplayKit
+import Foundation
 
 class GameScene: SKScene
 {
     
 }
-
-struct Timer{
-    var time = 60//This is the set time they are allowed to play the game
-    //this function is the core to the count down timer loop
-    //(this should be placed in a loop in view controller)
-    mutating func countDown(penalty: Int){
-        time = time-(penalty)
-    }
-}
-
-struct Points{
-    var score = 0
-    
-    // below lays a way to score mutiple points for not hitting other objects
-    func hitStreakMutitiplier(hits: Int) -> Double{
-        // if they get 3 in a row with out missing they get times and a half points
-        // they get 10  points per mole they hit
-        // max mutipler they get is 5 once they hit more then 10 in a row
-        switch hits {
-        case 2...4:
-            return 1.5
-        case 5...8:
-            return 2
-        case 5...8:
-            return 3.5
-        default:
-            return 1
-        }
-    }
-    mutating func changeScoreMutltiplier(hitStreak:Int){
-        self.score += (Int)(hitStreakMutitiplier(hits: hitStreak)*10)
-    }
-    
-    func getMultiplier(hits: Int)-> Double{
-        return hitStreakMutitiplier(hits: hits)
-    }
-}
-
--- Groundhog Day/GameViewController.swift
+++ Groundhog Day/GameViewController.swift
@@ -18,29 +18,137 @@ class GameViewController: UIViewController
     @IBOutlet var viewArray: [UIView]!
     @IBOutlet var moleImageArray: [UIImageView]!
     
+    @IBOutlet var timerLabel: UILabel!
+    
+    var moleObj: [Pit] = []
+    
+    var seconds = 60 //This is the set time they are allowed to play the game
+    var difficulty: Double = 1 //Can be 1(easy), 2(medium), or 4(hard)
+    var numberOfMoles: Double = 2 //Cannot be more than 15
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        for mole in moleImageArray
+        for (index, mole) in moleImageArray.enumerated()
         {
             mole.isHidden = true
+            moleObj.append(Pit.init(index, difficulty))
         }
+        
+        _ = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+        
+        _ = Timer.scheduledTimer(timeInterval: difficulty/numberOfMoles/10.0, target: self, selector: #selector(checkTimeRemaining), userInfo: nil, repeats: true)
+        
+        _ = Timer.scheduledTimer(timeInterval: difficulty/numberOfMoles, target: self, selector: #selector(addMole), userInfo: nil, repeats: true)
         
         //Turn phone into landscape
         //let value = UIInterfaceOrientation.landscapeRight.rawValue
         //UIDevice.current.setValue(value, forKey: "orientation")
     }
     
+    @objc func checkTimeRemaining()
+    {
+        var failSafe = Int() //How many moles are on the screen
+        var leastIndex = Int() //Mole with the lowest time left
+        var leastLife: Double = 1/difficulty //Index of the mole with the lowest time left
+        
+        for (index, obj) in moleObj.enumerated()
+        {
+            if obj.hasHog
+            {
+                failSafe += 1
+                
+                if obj.timeRemaining <= leastLife
+                {
+                    leastLife = obj.timeRemaining
+                    leastIndex = index
+                }
+            }
+        }
+        
+        if failSafe > Int(numberOfMoles)
+        {
+            moleObj[leastIndex].hasHog = false
+            moleObj[leastIndex].timeRemaining = 1/difficulty
+            checkMoles()
+        }
+        
+        for obj in moleObj
+        {
+            if obj.hasHog
+            {
+                obj.timeRemaining -= difficulty/numberOfMoles/10.0
+                
+                if obj.timeRemaining <= 0
+                {
+                    obj.hasHog = false
+                    obj.timeRemaining = 1/difficulty
+                    checkMoles()
+                }
+            }
+        }
+    }
+    
+    func checkMoles()
+    {
+        for (index, obj) in moleObj.enumerated()
+        {
+            if obj.hasHog
+            {
+                moleImageArray[index].isHidden = false
+            }
+            
+            else
+            {
+                moleImageArray[index].isHidden = true
+            }
+        }
+    }
+    
+    @objc func countDown()
+    {
+        seconds -= 1
+        timerLabel.text = "\(seconds)s"
+    }
+    
+    @objc func addMole()
+    {
+        var rand: Int
+        
+        repeat
+        {
+            rand = Int(arc4random_uniform(15))
+        } while moleObj[rand].hasHog
+        
+        moleObj[rand].hasHog = true
+        checkMoles()
+    }
+    
+    func penalize(penalty: Int)
+    {
+        seconds -= penalty
+        timerLabel.text = "\(seconds)s"
+    }
+    
     @IBAction func tapPit(_ sender: UIButton) //If the player taps the pit
     {
         
     }
+    
+    class Pit
+    {
+        var index: Int //Index of buttonArray, viewArray, moleImageArray
+        var hasHog: Bool //If a hog is visibile in a pit
+        var timeRemaining: Double //Time for the mole to be on the screen
+        
+        init(_ ind: Int, _ diff: Double)
+        {
+            index = ind
+            hasHog = false
+            timeRemaining = 2.0/diff
+        }
+    }
 }
 
-class Pit
-{
-    var index = Int() //Index of buttonArray, viewArray, moleImageArray
-    var hasHog = Bool() //If a hog is visibile in a pit
 
-}
